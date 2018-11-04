7349560008a95bd7504aea830b498d8957876976
Lukas R <lureiche@stud.hs-heilbronn.de>
2018-11-04 15:38:28 +0100

    Komentare

--- src/PermOneThree.java
+++ src/PermOneThree.java
@@ -1,93 +1,139 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Algorithmen Uebung 4.
+ *
+ * @author Tobias Müller              193683
+ * @author Peter Tim Oliver Nauroth   198322
+ * @author Lukas Reichert             199034
+ */
 public class PermOneThree {
-  static List<List<Integer>> data;
+  private List<List<Integer>> data = new ArrayList<>();
 
-  public static void main(String[] args) {
-    data = new ArrayList<>();
-    int n;
+  /**
+   * Konstruktor.
+   */
+  public PermOneThree() {
+    // Scanner zum einlesen erstellen.
     Scanner input = new Scanner(System.in);
 
-    System.out.print("Bitte geben Sie N ein: ");
-    n = input.nextInt();
-    newstartPermute(n);
+    try {
+      int n;
+      // Hinweis geben und Input einlesen.
+      System.out.print("Bitte geben Sie N ein: ");
+      n = input.nextInt();
 
+      // Beginne die permutation.
+      newStartPermute(n);
 
-
-    //Collections.sort(data);
-
-    Collections.sort(data,
-            (o1, o2) ->{
-
-              for(int i = 0; i < o1.size(); i++) {
-                if(o1.get(i)>o2.get(i)) {
-                  return 1;
-                }else if(o1.get(i)<o2.get(i)) {
-                  return -1;
-                }
-
+      // Sortiere die Permutationen.
+      Collections.sort(data,
+          (o1, o2) -> {
+            for (int i = 0; i < o1.size(); i++) {
+              if (o1.get(i) > o2.get(i)) {
+                return 1;
+              } else if (o1.get(i) < o2.get(i)) {
+                return -1;
               }
-              return 0; //Passiert in dem fall nie but w/e
-            });
+            }
+            return 0; //Passiert in dem fall nie aber w/e.
+          });
 
-    data.forEach(t -> System.out.println(t));
-    System.out.println("Es gab genau "+ data.size() +" Permutationen der verlangten Art.");
+      // Gib all permuationen aus.
+      data.forEach(t -> System.out.println(t));
+      System.out.println("Es gab genau " + data.size() + " Permutationen der verlangten Art.");
+
+    }catch (Exception e) {
+      System.out.println("Ungueltige Eingabe");
+    }
   }
 
-  private static void newstartPermute(int n) {
+  /**
+   * Beginnt die Permuation.
+   *
+   * @param n Maximal Zahl
+   */
+  private void newStartPermute(int n) {
+    // Von 1 bis n gehen.
     for(int i =1 ; i<=n;i++) {
       List<Integer> test = new ArrayList<>();
       test.add(i);
       newPermute(test,n);
     }
+  }
 
+  /**
+   * Neue Permutation einfügen.
+   *
+   * @param list2 Liste mit der derzeitigen Position
+   * @param n Maximal Zahl
+   */
+  private void newPermute(List<Integer> list2 ,int n) {
+    List<Integer> list=new ArrayList<>();
+    // Werte kopieren
+    list.addAll(list2);
+    // Überprüfen ob alle Zahlen beinhaltet sind.
+    if(list.size()==n) {
+      // Füge es der "End" Liste hinzu.
+      data.add(list);
+    }else {
+      // Überprüfen ob die letzte Zahl passend ist (-4).
+      if(!list.contains(list.get(list.size()-1)-4)&&list.get(list.size()-1)-4>0) {
+        List<Integer> templist = new ArrayList<>();
+        // Liste hinzufügen und ...
+        templist.addAll(list);
+        // ... letztes element hinzufügen.
+        templist.add(list.get(list.size()-1)-4);
+        // Rekursiver aufruf mit der neuen Liste.
+        newPermute(templist,n);
+      }
 
+      // Überprüfen ob die letzte Zahl passend ist (+1).
+      if(!list.contains(list.get(list.size()-1)+1)&&list.get(list.size()-1)+1<=n) {
+        List<Integer> templist = new ArrayList<>();
+        // Liste hinzufügen und ...
+        templist.addAll(list);
+        // ... letztes element hinzufügen.
+        templist.add(list.get(list.size()-1)+1);
+        // Rekursiver aufruf mit der neuen Liste.
+        newPermute(templist,n);
+      }
+
+      // Überprüfen ob die letzte Zahl passend ist (+4).
+      if(!list.contains(list.get(list.size()-1)+4)&&list.get(list.size()-1)+4<=n) {
+        List<Integer> templist = new ArrayList<>();
+        // Liste hinzufügen und ...
+        templist.addAll(list);
+        // ... letztes element hinzufügen.
+        templist.add(list.get(list.size()-1)+4);
+        // Rekursiver aufruf mit der neuen Liste.
+        newPermute(templist,n);
+      }
+
+      // Überprüfen ob die letzte Zahl passend ist (-1).
+      if(!list.contains(list.get(list.size()-1)-1)&&list.get(list.size()-1)-1>0) {
+        List<Integer> templist = new ArrayList<>();
+        // Liste hinzufügen und ...
+        templist.addAll(list);
+        // ... letztes element hinzufügen.
+        templist.add(list.get(list.size()-1)-1);
+        // Rekursiver aufruf mit der neuen Liste.
+        newPermute(templist,n);
+      }
+    }
 
   }
 
-  private static void newPermute(List<Integer> list2 ,int n) {
-    List<Integer> list=new ArrayList<>();
-    list.addAll(list2);
-    if(list.size()==n) {
-      data.add(list);
-    }else {
-
-      if(!list.contains(list.get(list.size()-1)-4)&&list.get(list.size()-1)-4>0) {
-        List<Integer> templist = new ArrayList<>();
-        templist.addAll(list);
-        templist.add(list.get(list.size()-1)-4);
-        newPermute(templist,n);
-      }
-
-      if(!list.contains(list.get(list.size()-1)+1)&&list.get(list.size()-1)+1<=n) {
-        List<Integer> templist = new ArrayList<>();
-        templist.addAll(list);
-        templist.add(list.get(list.size()-1)+1);
-        newPermute(templist,n);
-      }
-
-
-      if(!list.contains(list.get(list.size()-1)+4)&&list.get(list.size()-1)+4<=n) {
-        List<Integer> templist = new ArrayList<>();
-        templist.addAll(list);
-        templist.add(list.get(list.size()-1)+4);
-        newPermute(templist,n);
-      }
-
-      if(!list.contains(list.get(list.size()-1)-1)&&list.get(list.size()-1)-1>0) {
-        List<Integer> templist = new ArrayList<>();
-        templist.addAll(list);
-        templist.add(list.get(list.size()-1)-1);
-        newPermute(templist,n);
-      }
-
-
-    }
-
+  /**
+   * Main Methode um das Programm zu starten.
+   *
+   * @param args Start Argumente
+   */
+  public static void main(String[] args) {
+    new PermOneThree();
   }
 
 }
--- src/WalkNodes.java
+++ src/WalkNodes.java
@@ -1,5 +1,7 @@
 import java.math.BigInteger;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Algorithmen Uebung 4.
@@ -10,8 +12,7 @@ import java.util.*;
  */
 public class WalkNodes {
 
-  private Map<String, BigInteger[]> memory;
-  private BigInteger steps;
+  private Map<String, BigInteger> memory;
 
   private enum Direction {
     RIGHT,      // →
@@ -22,6 +23,9 @@ public class WalkNodes {
     DEFAULT,    // Tu nichts (Fuer den Start)
   }
 
+  /**
+   * Konstruktor.
+   */
   public WalkNodes() {
     Scanner s = new Scanner(System.in);
 
@@ -29,27 +33,22 @@ public class WalkNodes {
     while (true) {
       String input;
       try {
+        // Eingabe einlesen.
         input = s.nextLine();
 
+        //Überprüfen ob man rausgehen soll.
         if (input.equals("hguone")) {
           System.out.println("bye");
           return;
         }
+        // Zu int konvertieren.
         int n = Integer.parseInt(input);
 
         memory = new HashMap<>();
-        this.steps = BigInteger.ZERO;
-
+        // Punkte zählen.
         print(n, walk(n, 0, 0, Direction.DEFAULT, BigInteger.ZERO));
-
-//        Object[] c =  memory.keySet().toArray();
-//
-//        for (int i = 0; i < c.length; i++) {
-//          System.out.println(c[i] + "  " + memory.get(c[i]));
-//        }
       } catch (Exception e) {
-        e.printStackTrace();
-        System.out.println("Unguelitige Eingabe!");
+        System.out.println("Unguelitige Eingabe.");
       }
     }
   }
@@ -57,84 +56,68 @@ public class WalkNodes {
   /**
    * Rekurive funktion fuer das laufen.
    *
-   * @param n int
-   * @param x int
-   * @param y int
-   * @param d Direction
+   * @param n     int
+   * @param x     int
+   * @param y     int
+   * @param d     Direction
+   * @param steps BigInteger
    * @return BigInteger
    */
-  private BigInteger[] walk(int n, int x, int y, Direction d, BigInteger steps) {
+  private BigInteger walk(int n, int x, int y, Direction d, BigInteger steps) {
     steps = steps.add(BigInteger.ONE);
 
     // Ueberfluessige koennen von anfang an nicht beachtet werden.
     if (x < 0 || y < 0 || x > n || y > n || n - x - y < 0) {
-      return new BigInteger[] {BigInteger.ZERO, BigInteger.ZERO};
+      return BigInteger.ZERO;
     }
     // Ueberpruefen ob man am ziel angekommen ist.
     if (x == n && y == 0) {
-      return new BigInteger[] {BigInteger.ONE, steps};
+      return steps;
     }
 
     // Ueberpruefen ob der Wert schon im cache liegt. (D.P.)
-    String compare = x + "|" + y + "|" + d + "|" + n + "|" + steps;
+    String compare = d.toString() + "|" + x + "|" + y + "|" + steps;
     if (memory.containsKey(compare)) {
       return memory.get(compare);
     }
 
-    BigInteger resultPaths;
-    BigInteger resultSteps;
+    BigInteger result;
 
     // Einschraenkungen beachten und rekursiv weiter laufen.
     switch (d) {
       case UP:
-        BigInteger[] i = walk(n, x - 1, y + 1, Direction.DOWN_RIGHT, steps);
-        BigInteger[] j = walk(n, x, y + 1, Direction.UP, steps);
-        BigInteger[] k = walk(n, x + 1, y - 1, Direction.UP_LEFT, steps);
-
-        resultPaths = i[0].add(j[0]).add(k[0]);
-        resultSteps = i[1].add(j[1]).add(k[1]);
+        result = walk(n, x - 1, y + 1, Direction.DOWN_RIGHT, steps)
+            .add(walk(n, x, y + 1, Direction.UP, steps))
+            .add(walk(n, x + 1, y - 1, Direction.UP_LEFT, steps));
         break;
       case UP_LEFT:
-        BigInteger[] iu = walk(n, x + 1, y, Direction.RIGHT, steps);
-        BigInteger[] ju = walk(n, x, y + 1, Direction.UP, steps);
-        BigInteger[] ku = walk(n, x + 1, y - 1, Direction.UP_LEFT, steps);
-        BigInteger[] lu = walk(n, x + 1, y + 1, Direction.UP_RIGHT, steps);
-
-        resultPaths = iu[0].add(ju[0]).add(ku[0]).add(lu[0]);
-        resultSteps = iu[1].add(ju[1]).add(ku[1]).add(lu[1]);
+        result = walk(n, x + 1, y - 1, Direction.UP_LEFT, steps)
+            .add(walk(n, x + 1, y, Direction.RIGHT, steps))
+            .add(walk(n, x, y + 1, Direction.UP, steps))
+            .add(walk(n, x + 1, y + 1, Direction.UP_RIGHT, steps));
         break;
       case RIGHT:
       case UP_RIGHT:
-        BigInteger[] ir = walk(n, x + 1, y, Direction.RIGHT, steps);
-        BigInteger[] jr = walk(n, x - 1, y + 1, Direction.DOWN_RIGHT, steps);
-        BigInteger[] kr = walk(n, x + 1, y - 1, Direction.UP_LEFT, steps);
-        BigInteger[] lr = walk(n, x + 1, y + 1, Direction.UP_RIGHT, steps);
-
-        resultPaths = ir[0].add(jr[0]).add(kr[0]).add(lr[0]);
-        resultSteps = ir[1].add(jr[1]).add(kr[1]).add(lr[1]);
+        result = walk(n, x + 1, y - 1, Direction.UP_LEFT, steps)
+            .add(walk(n, x + 1, y, Direction.RIGHT, steps))
+            .add(walk(n, x - 1, y + 1, Direction.DOWN_RIGHT, steps))
+            .add(walk(n, x + 1, y + 1, Direction.UP_RIGHT, steps));
         break;
       case DOWN_RIGHT:
-        BigInteger[] id = walk(n, x + 1, y, Direction.RIGHT, steps);
-        BigInteger[] jd = walk(n, x - 1, y + 1, Direction.DOWN_RIGHT, steps);
-        BigInteger[] kd = walk(n, x, y + 1, Direction.UP, steps);
-        BigInteger[] ld = walk(n, x + 1, y + 1, Direction.UP_RIGHT, steps);
-
-        resultPaths = id[0].add(jd[0]).add(kd[0]).add(ld[0]);
-        resultSteps = id[1].add(jd[1]).add(kd[1]).add(ld[1]);
+        result = walk(n, x - 1, y + 1, Direction.DOWN_RIGHT, steps)
+            .add(walk(n, x + 1, y, Direction.RIGHT, steps))
+            .add(walk(n, x, y + 1, Direction.UP, steps))
+            .add(walk(n, x + 1, y + 1, Direction.UP_RIGHT, steps));
         break;
       default:
-        BigInteger[] ide = walk(n, x + 1, y, Direction.RIGHT, steps);
-        BigInteger[] jde = walk(n, x - 1, y + 1, Direction.DOWN_RIGHT, steps);
-        BigInteger[] kde = walk(n, x, y + 1, Direction.UP, steps);
-        BigInteger[] lde = walk(n, x + 1, y + 1, Direction.UP_RIGHT, steps);
-        BigInteger[] mde = walk(n, x + 1, y - 1, Direction.UP_LEFT, steps);
-
-        resultPaths = ide[0].add(jde[0]).add(kde[0]).add(lde[0]).add(mde[0]);
-        resultSteps = ide[1].add(jde[1]).add(kde[1]).add(lde[1]).add(mde[1]);
+        result = walk(n, x + 1, y - 1, Direction.UP_LEFT, steps)
+            .add(walk(n, x + 1, y, Direction.RIGHT, steps))
+            .add(walk(n, x, y + 1, Direction.UP, steps))
+            .add(walk(n, x - 1, y + 1, Direction.DOWN_RIGHT, steps))
+            .add(walk(n, x + 1, y + 1, Direction.UP_RIGHT, steps));
         break;
     }
 
-    BigInteger[] result = new BigInteger[]{resultPaths, resultSteps};
     // Ergebnis im cache speichern. (D.P.)
     memory.put(compare, result);
 
@@ -144,11 +127,11 @@ public class WalkNodes {
   /**
    * Ausdrucken in der Konsole.
    *
-   * @param n     int
-   * @param paths BigInteger
+   * @param n      int
+   * @param points BigInteger
    */
-  private void print(int n, BigInteger[] paths) {
-    System.out.println("Steps: " + paths[1] + " | (" + n + ", " + paths[0] + ")");
+  private void print(int n, BigInteger points) {
+    System.out.println("n=" + n + " -> Punkte: " + points);
   }
 
   /**
